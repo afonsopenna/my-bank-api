320611d8d23cea38b674229c429fd683fbccc3ba
Thiago Penna <afonsopenna@gmail.com>
2020-06-18 01:52:13 -0300

    Fim da aula POST

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-console.log("hello world");
+import express from "express";
+import fs from "fs";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post("/account", (req, res) => {
+  let account = req.body;
+
+  fs.readFile("accounts.json", "utf-8", (err, data) => {
+    if (!err) {
+      try {
+        let json = JSON.parse(data);
+        account = { id: json.nextId++, ...account };
+        json.accounts.push(account);
+        fs.writeFile("accounts.json", JSON.stringify(json), (err) => {
+          if (err) {
+            res.status(400).send({ error: err.message });
+          } else {
+            res.send("POST Account");
+          }
+        });
+      } catch (err) {
+        res.status(400).send({ error: err.message });
+      }
+    } else {
+      res.status(400).send({ error: err.message });
+    }
+  });
+});
+
+app.listen(port, () => {
+  try {
+    fs.readFile("accounts.json", "utf-8", (err, data) => {
+      if (err) {
+        const initialJson = {
+          nextId: 1,
+          accounts: [],
+        };
+        fs.writeFile("accounts.json", JSON.stringify(initialJson), (err) => {
+          if (err) {
+            console.log("Arquivo criado com sucesso!");
+          }
+        });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
